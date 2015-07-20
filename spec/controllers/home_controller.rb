beda5a4d8a77a42c7524e903397c226fb72ca86c
kkane106 <kkane106@gmail.com>
2015-07-20 13:52:29 -0400

    Update rspec test to include status message of response

--- spec/controllers/home_controller.rb
+++ spec/controllers/home_controller.rb
@@ -1,13 +1,19 @@
-RSpec.describe HomeController do
-  describe "GET #show " do
-    it "Returns json" do
-      get :show
-      response.header['Content-Type'].should include 'text/javascript'
-    end
+require "rails_helper"
 
+RSpec.describe HomeController, :type => :controller do
+  describe "GET #show " do
+    it "responds successfully with a 200 status code" do
+      get :show
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
     it "renders the show template" do
       get :show
       expect(response).to render_template("show")
+    end
+     it "Returns json" do
+      get :show
+      response.header['Content-Type'].should include 'text/javascript'
     end
   end
 end
